be0eecb48a798d64928139faf9ad93a05ef2b223
Remi Mata <remi.mata@ecam.fr>
2021-11-21 23:45:20 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="component">
     <div id="nav">
+      <img src="../assets/postit_image.png" alt="" class="logo" />
+      <div class="search-container">
+        <input type="text" /><button>Search</button>
+      </div>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/Contact">Contact Us</router-link>
+      <input
+        type="image"
+        id="logo_profile"
+        name="user"
+        src="./profil.png"
+        alt="User Picture"
+        width="32"
+        height="32"
+      />
     </div>
-    
   </div>
 </template>
 
@@ -16,13 +28,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.component{
-    position:sticky;
-    top:0;
-    display:flex;
-    justify-content:center;
-    width:100%;
-    background-color: rgba(0, 0, 0, 0.719);
+.component {
+  position: sticky;
+  top: 0;
+
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.719);
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -33,7 +44,23 @@ export default {
 }
 
 #nav {
-  padding: 30px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  display: flex;
+  align-items: center;
+  .search-container {
+    margin-right: 20%;
+    input {
+      background-color: rgba(255, 255, 255, 0.418);
+      border-radius: 10px;
+      width: 250px;
+    }
+    button {
+      color: white;
+      background-color: rgba(255, 255, 255, 0.418);
+      border-radius: 10px;
+    }
+  }
 
   a {
     font-weight: bold;
@@ -42,6 +69,13 @@ export default {
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+  .logo {
+    margin-left:10px;
+    height: 40px;
+  }
+  #logo_profile {
+    margin-left: 35%;
   }
 }
 </style>
