4c3a207731b13f180d2a4cee3867115b86c86033
Auroberfroid <aurelien.roberfroid@gmail.com>
2021-11-21 22:59:26 +0200

    css and html of contact, about us not finished yet

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p id='title'>About us</p>
+    <p>We are currently developping our web application PostIt !</p>
+    <p>It allows people to add post to a page, some kind of social media but on post it notes.</p>
+    <p>For now you can navigate threw the webpage, but the really important page is Main, you can add postIt, like others ones (or yours...) and reset the like count</p>
+    <p>Thank you for using our application, we'll keep working on it and try to make it better with time !</p>
   </div>
 </template>
 
@@ -12,3 +16,7 @@ export default {
   
 }
 </script>
+<style lang="scss" scoped>
+
+
+</style>
--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-      coucou
+  <div id = 'contactUs'>
+    <p id = 'title'>Contact Us</p>
+    <div id = 'frame'>
+      <br><p class = 'type_mail'>Email : </p><p class = 'mail'> bestwadteam@ut.ee</p>
+      <br><p class = 'type_phone'>Phone number : </p><p class = 'phone'> 0874589132</p><br>
+    </div>
   </div>
 </template>
 
@@ -10,6 +14,60 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#title
+{
+  text-align: center;
+  font-size: 50px;
+  font : bold;
+
+}
+
+#frame
+{
+  /*margin-top: 10%;*/
+  margin-left: auto;
+  margin-right: auto;
+  border-width: 3px;
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 6px 16px;
+  width: 580px;
+  background-color: #42b983;
+  }
+
+.type_mail
+{ 
+  margin-top: 0px;
+  text-align: center;
+  color: auto;
+  font-size: 35px;
+  font-weight: bold;
+}
+
+.type_phone
+{
+  text-align: center;
+  color: auto;
+  font-size: 35px;
+  font-weight: bold;
+}
+     
+.mail
+{ 
+  text-align: center;
+  color: auto;
+  font-size: 35px;
+  font-weight:lighter;
+}
+
+.phone
+{
+  margin-bottom: 0px;
+  text-align: center;
+  color: auto;
+  font-size: 35px;
+  font-weight:lighter;
+}
+
+
 
 </style>
